ff1796decfab8cc5465a55158e993fcccffbb0f8
ryunzz <ryan.alexander.ni@gmail.com>
2024-07-28 01:03:53 +0900

    landing initial commit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Landing from "@/components/Landing";
 
 const page = () => {
   return (
     <div className='Home mt-8'>
       <h1>HomPage Content goes here</h1>
+
+      <Landing />
     </div>
   )
 }
