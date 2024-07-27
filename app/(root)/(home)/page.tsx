39d7032f6cd729be04f72fa89357d4caad365c9a
ryunzz <ryan.alexander.ni@gmail.com>
2024-07-28 05:17:04 +0900

    removed more content redundency

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Landing from "@/components/Landing";
+import Landing from "../../components/Landing";
 
 const page = () => {
   return (
     <div className='Home mt-8'>
-      <h1>HomPage Content goes here</h1>
 
       <Landing />
     </div>
